b48ffea389e5f1e73127f7dc9e9459be061318d5
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2024-08-26 09:26:45 +0300

    lesson2-2/3

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -5,5 +5,19 @@
  */
 
 const score = 70 // тестовое значение, можно изменять
-let grade
+let grade = Math.floor(Math.random()*101)
 // your code
+
+    if(grade < 50) {
+        console.log("F");
+    }else if(grade < 70){
+        console.log("D");
+    }else if(grade < 80){
+        console.log("C");
+    }else if(grade < 90){
+        console.log("B");
+    }else {
+        console.log("A");
+    }
+
+console.log(grade);
