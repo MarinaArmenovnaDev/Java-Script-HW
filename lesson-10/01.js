e024c8ad26e093db91ab53d110fe5bb238e12924
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2024-10-19 21:38:45 +0300

    fix

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -29,6 +29,10 @@ const model = {
     this.movies.push(newMovie)
     view.renderMovies(this.movies)
   },
+  deleteMovie(id){
+    this.movies = this.movies.filter(movie => movie.id !== id)
+    view.renderMovies(this.movies)
+  }
   // your code
 }
 
@@ -39,6 +43,7 @@ const view = {
     const form = document.querySelector('.form')
     const inputTitle = document.querySelector('.input-title')
     const inputDescription = document.querySelector('.input-description')
+    const list = document.querySelector('.list')
 
     form.addEventListener('submit', function (event) {
       event.preventDefault()
@@ -50,7 +55,12 @@ const view = {
       inputDescription.value = ''
     })
 
-    // your code
+   list.addEventListener('click', function(event){
+      if(event.target.classList.contains('delete-button')){
+        const movieId = +event.target.parentElement.id;
+        controller.deleteMovie(movieId)
+      }
+    })
   },
   renderMovies(movies) {
     const list = document.querySelector('.list')
@@ -90,6 +100,11 @@ const controller = {
       view.displayMessage('Заполните все поля!', true)
     }
   },
+
+  deleteMovie(id){
+    model.deleteMovie(id)
+    view.displayMessage('Фильм успешно удалён!')
+  }
   // your code
 }
 
